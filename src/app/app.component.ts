775065da739aa48ca92cd455ea18d213746e55d3
helderlim <helderfelima16@gmail.com>
2021-02-16 09:35:28 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  //esta linkando meu html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'Seja bem-vindo!';
+
+  constructor(){
+
+  }
+
+ 
 }
