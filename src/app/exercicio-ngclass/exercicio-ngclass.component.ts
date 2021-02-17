4f312dbb91da462f458d5fe1d7d4f4815e5baee1
helderlim <helderfelima16@gmail.com>
2021-02-17 13:34:37 -0300

    feat: DIRETIVAS ngClass

---
+++ src/app/exercicio-ngclass/exercicio-ngclass.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio-ngclass',
+  templateUrl: './exercicio-ngclass.component.html',
+  styleUrls: ['./exercicio-ngclass.component.scss']
+})
+export class ExercicioNgclassComponent  {
+
+  deveSerVerde = false;
+
+  tornarVerde(){
+    this.deveSerVerde = true;
+  }
+
+}
