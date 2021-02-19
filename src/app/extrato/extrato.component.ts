614a949600a6cc307ab816a3d47b3efc633151f8
helderlim <helderfelima16@gmail.com>
2021-02-18 23:00:23 -0300

    feat:  Loading e Tratamento de Erro

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -13,15 +13,30 @@ export class ExtratoComponent implements OnInit {
 
   transacoes!: Array<Transacao>;
 
+  estaCarregando!: boolean;
+
+  erroNoCarregamento!: boolean;
 
 
   constructor(private extratoService: ExtratoService) { 
   }
 
   ngOnInit(){
+    this.carregarExtrato();
+  }
+
+  carregarExtrato(){
+    this.estaCarregando = true;
+    this.erroNoCarregamento= false;
     this.extratoService.getTransacoes()
     .subscribe(response=>{
+    this.estaCarregando = false;
       this.transacoes = response;
+    }, error=>{
+      //fazer algo se der erro ,
+      this.estaCarregando = false;
+      this.erroNoCarregamento= true;
+      
     })
   }
 
