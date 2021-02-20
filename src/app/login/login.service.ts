9567762ac7b37fc6abe2fdb5be211037e3a18a72
helderlim <helderfelima16@gmail.com>
2021-02-20 08:03:43 -0300

    feat: Aprimorando Service de Autenticacao

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { of, throwError, timer } from 'rxjs';
+import { Observable, of, throwError, timer } from 'rxjs';
 import { delay, mergeMap, tap } from 'rxjs/operators';
 
 import { AuthService } from '../shared/services/auth/auth.service';
+import { LoginResponse } from './login.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class LoginService {
     private authService: AuthService,
   ) { }
 
-  logar(email: string, senha: string){
+  logar(email: string, senha: string): Observable<LoginResponse>{
     //return this.post(this.API_URL + '/auth', {})
     if (email === 'helderfelima16@gmail.com' && senha === '123'){
       return of({
@@ -26,12 +27,11 @@ export class LoginService {
         token: 'DKFNSO32NO54JN3I5N4%V%v5',
       }).pipe(
         delay(2000),
-        tap(response =>{
-          
+        tap(response =>{   
           
         this.authService.setUsuario(response.usuario);
-         
-         
+        this.authService.setToken(response.token);
+
         })
       )
     }
