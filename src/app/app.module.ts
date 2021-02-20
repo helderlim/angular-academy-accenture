7887f97e45f6fac5f09d180cfcbb4d354ae30c86
helderlim <helderfelima16@gmail.com>
2021-02-19 21:13:00 -0300

    feat: ngForm, ngSubmit, e ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import localePt from '@angular/common/locales/pt';
 import { LOCALE_ID, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -32,6 +33,7 @@ registerLocaleData(localePt, 'pt')
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     SharedModule,
   ],
   providers: [{
