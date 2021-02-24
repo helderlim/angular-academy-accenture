a7512e93abb7f05bb4f773fd44196d8072a0f4c9
helderlim <helderfelima16@gmail.com>
2021-02-24 20:05:57 -0300

    feat: Aplicando classes de CSS

--- src/app/area-logada/contatos/novo-contato/novo-contato.component.ts
+++ src/app/area-logada/contatos/novo-contato/novo-contato.component.ts
@@ -17,17 +17,34 @@ export class NovoContatoComponent implements OnInit {
   ngOnInit(){
     // this.contatoForm = new FormGroup({
     //   nome: new FormControl(),
+    //   email: new FormControl(),
     //   banco: new FormControl(),
     // });
 
     this.contatoForm = this.formBuilder.group({
       nome: ['', Validators.required],
-      banco: ['', Validators.required, Validators.minLength(3)],
-      email: ['', Validators.email, Validators.required],
+      banco: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.email, Validators.required]],
     })
   }
+  exibeErro(nomeControle: string) {
+    if (!this.contatoForm.controls[nomeControle]) {
+      return false;
+    }
+    return this.contatoForm.controls[nomeControle].invalid && this.contatoForm.controls[nomeControle].touched;
+  }
 
+  validateAllFormFields(){
+    Object.keys(this.contatoForm.controls).forEach(field => {
+     const control =  this.contatoForm.controls[field];
+     control.markAsTouched();
+    })
+  }
   salvarContato(){
+    if (this.contatoForm.invalid) {
+      this.validateAllFormFields();
+      return;
+    }
     console.log(this.contatoForm);
     
   }
